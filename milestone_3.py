d667fc8acc387a1181d0e4e4f587ae266df85d0b
Bloggs241 <keith.johnstone@live.co.uk>
2025-02-27 17:42:11 +0000

    created and updated milestone_3.py

---
+++ milestone_3.py
@@ -0,0 +1,14 @@
+import random
+word_list = ["apple", "banana", "cherry", "dragonfruit", "eggplant"]
+word = random.choice(word_list)
+while True:
+	guess = input("Please guess a letter")
+	if guess.isalpha==True:
+		break
+	else:
+		print("Invalid letter. Please, enter a single alphabetical character.")
+	for letter in word:
+		if guess in word:
+			print(f"Good guess, {guess} is in the word")
+	else:
+		print(f"Sorry, {guess} is not in the word. Try again.")
